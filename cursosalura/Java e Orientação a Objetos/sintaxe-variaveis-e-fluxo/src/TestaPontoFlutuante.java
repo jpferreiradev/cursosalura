6bc6be8bf51c496a59ab0635d8ce5a52ead3d82f
JP Ferreira <jotepauder01@gmail.com>
2021-04-23 16:51:25 -0300

    Add novas classes na carreira Java

---
+++ cursosalura/Java e Orientação a Objetos/sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,29 @@
+
+public class TestaPontoFlutuante {
+	
+		public static void main(String[] args ) {
+			
+			double salario;
+			salario = 1250.70;
+			
+			System.out.println("Meu salário é de: " + salario);
+	
+			double idade = 29;
+			
+			double divisao = 3.14 / 2;
+			System.out.println(divisao);
+			
+			// Inteiro  + Inteiro = inteiro
+			int outraDivisao = 5 / 2;
+			System.out.println(outraDivisao);
+
+			
+			double outraTentativa = 5.0 / 2;
+			System.out.println(outraTentativa);
+			
+			/* declarando variaveis
+			 double salario = 1250.70
+			 declara a variavel + fala qual é o tipo dela + faz uma atribuição 
+			 */
+		}
+}
---
+++ cursosalura/Java e Orientação a Objetos/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,21 @@
+
+public class TestaVariaveis {
+	
+		public static void main (String[] args) {
+			
+			int idade;
+			idade = 29;
+				
+			System.out.println("A minha idade é: " + idade + " anos" );
+			
+			idade = 20 + 9;
+			
+			System.out.println(idade);
+			
+			idade = (3 * 3) + 20;
+			
+			System.out.println(idade);
+			
+		
+		}	
+}
