ae8596393a0f6cec25310a7d7d6865da788decb4
JP Ferreira <jotepauder01@gmail.com>
2021-04-25 13:15:27 -0300

    Update curso de Java

--- cursosalura/Java e Orientação a Objetos/sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
+++ cursosalura/Java e Orientação a Objetos/sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -21,6 +21,10 @@ public class TestaPontoFlutuante {
 			double outraTentativa = 5.0 / 2;
 			System.out.println(outraTentativa);
 			
+			double outraTentativa2 = 6.0 / 2;
+			System.out.println(outraTentativa2);
+			
+			
 			/* declarando variaveis
 			 double salario = 1250.70
 			 declara a variavel + fala qual é o tipo dela + faz uma atribuição 
