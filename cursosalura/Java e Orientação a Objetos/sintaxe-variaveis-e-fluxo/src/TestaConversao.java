41a5c517d24ff1971b49fd3372d7a1b9fec807c5
JP Ferreira <jotepauder01@gmail.com>
2021-04-26 19:36:49 -0300

    Update curso java Alura

--- cursosalura/Java e Orientação a Objetos/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
+++ cursosalura/Java e Orientação a Objetos/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -16,9 +16,16 @@ public class TestaConversao {
 		double valor1 = 0.2;
 		double valor2 = 0.1;
 		double total = valor1 + valor2; 
-		System.out.println(total	);
+		System.out.println(total);
 		
-		//float pontoFlutuante = 3.14f;
+		float pontoFlutuante = 3.14f;
+		
+		/*double valor4 = 0.1;
+		double valor5 = 0.3;
+			double total1 = valor2 + valor5;
+		System.out.println(total1);
+		*/
+		
 		
 	}
 }
---
+++ cursosalura/Java e Orientação a Objetos/sintaxe-variaveis-e-fluxo/src/exemplo1.java
@@ -0,0 +1,13 @@
+
+public class exemplo1 {
+	
+	public static void main(String[] args){
+	
+	String parcela1 = "10";
+	String parcela2 = "20";
+	
+	System.out.println(parcela1 + parcela2);
+		
+		
+	}
+}
