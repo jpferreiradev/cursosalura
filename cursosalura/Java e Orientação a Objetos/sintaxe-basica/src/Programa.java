ff09e7273d396fc4a4a9da579a837d30a2ff50ab
JP Ferreira <jotepauder01@gmail.com>
2021-07-09 18:01:46 -0300

    Update Java

--- cursosalura/Java e Orientação a Objetos/sintaxe-basica/src/Programa.java
+++ cursosalura/Java e Orientação a Objetos/sintaxe-basica/src/Programa.java
@@ -1,7 +1,11 @@
 
 public class Programa {
 	
-	public static void main (String [] args) {
-		System.out.println("Olá, mundo");
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo");
+	
 	}
+		
 }
+
