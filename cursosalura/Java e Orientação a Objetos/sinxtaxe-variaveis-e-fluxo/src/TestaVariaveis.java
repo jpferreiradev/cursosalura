af31febdd1dd7f7d79899d023bf51c10eb233ac0
JP Ferreira <jotepauder01@gmail.com>
2021-07-10 10:46:00 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a Objetos/sinxtaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,30 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		
+		//System.out.println("Olá novo teste");
+		
+		
+		
+		int idade = 29;
+		
+		System.out.println(idade);
+		System.out.println("A minha idade é " + idade + " anos ");
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 5) + 2;
+		
+		System.out.println(idade);
+		
+		System.out.println(" A idade é " + idade);
+		
+		
+		
+		
+		
+	}
+}
