16ea2f3ee1b168a355f14c8df34159a2d09787e6
JP Ferreira <jotepauder01@gmail.com>
2021-07-11 14:44:53 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a Objetos/sinxtaxe-variaveis-e-fluxo/src/TesteConversao.java
@@ -0,0 +1,24 @@
+
+public class TesteConversao {
+	
+	public static void main(String [] args) {
+		double salario = 1270.50;
+		int valor = (int)salario;
+		System.out.println(valor);
+		
+		//var 64bits
+		long numeroGrande = 323232323232l;
+		short valorPequeno = 2131;
+		byte b = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);
+		
+		
+		
+		
+	}
+}
