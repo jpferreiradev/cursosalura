7f780dabdcf2110d13a778f9ba939eecf52aaf98
JP Ferreira <jotepauder01@gmail.com>
2021-07-12 17:14:14 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a Objetos/sinxtaxe-variaveis-e-fluxo/src/ExemploContatenacao.java
@@ -0,0 +1,17 @@
+
+public class ExemploContatenacao {
+	
+	
+	public static void main(String[] args) {
+		
+		String saudacao = "Olá, meu nome é ";
+		String nome = " JP";
+		String continuacao = " e minha idade é ";
+		int idade = 29;
+		String continuacao2 = " anos";
+		
+		System.out.println(saudacao+nome+continuacao+idade+continuacao2);
+		
+			
+	}
+}
--- cursosalura/Java e Orientação a Objetos/sinxtaxe-variaveis-e-fluxo/src/TesteConversao.java
+++ cursosalura/Java e Orientação a Objetos/sinxtaxe-variaveis-e-fluxo/src/TesteConversao.java
@@ -2,8 +2,10 @@
 public class TesteConversao {
 	
 	public static void main(String [] args) {
+		
 		double salario = 1270.50;
 		int valor = (int)salario;
+		
 		System.out.println(valor);
 		
 		//var 64bits
@@ -16,6 +18,18 @@ public class TesteConversao {
 		double total = valor1 + valor2;
 		
 		System.out.println(total);
+		
+		double valor3 = 0.5;
+		double valor4 = 1.5;
+		double valor5 = 2.5;
+		double totalNovo = valor3 + valor4 + valor5;
+		
+		System.out.println(totalNovo);
+		
+	
+		
+		
+		
 		
 		
 		
