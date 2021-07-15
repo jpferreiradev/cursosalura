9e951e3ef9a18d0a355ba9ad83e772b5e6ba5287
JP Ferreira <jotepauder01@gmail.com>
2021-07-15 16:32:37 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a Objetos/Atividades/src/MaiorNumero.java
@@ -0,0 +1,15 @@
+
+public class MaiorNumero {
+	public static void main(String[] args) {
+			
+		int numero1 = 25;
+		int numero2 = 15;
+		
+		if( numero1 > numero2) {
+			System.out.println(" O número 1 maior que número 2");
+			
+		} else {
+			System.out.println("O número 2 é menor que número 1");
+		}
+	}
+}
--- cursosalura/Java e Orientação a Objetos/Atividades/src/PrimeiroAtividade.java
+++
@@ -1,6 +0,0 @@
-
-public class PrimeiroAtividade {
-	public static void main(String[] args) {
-		
-	}
-}
