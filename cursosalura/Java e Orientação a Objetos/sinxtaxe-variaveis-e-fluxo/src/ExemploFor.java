344bc232481d9124e1085ae61b598ff16cdcab3b
JP Ferreira <jotepauder01@gmail.com>
2022-02-10 15:21:08 -0300

    Update

--- cursosalura/Java e Orientação a Objetos/sinxtaxe-variaveis-e-fluxo/src/ExemploFor.java
+++
@@ -1,9 +0,0 @@
-
-public class ExemploFor {
-			
-	public static void main(String[] args) {
-            for (int i = 0; i < 9; i++){
-                 System.out.println("Indice " + i);
-            }
-	}
-}
