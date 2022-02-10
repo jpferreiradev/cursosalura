18898fd6f1c5f958277ca2ea1253316c280ad10d
JP Ferreira <jotepauder01@gmail.com>
2022-02-10 15:26:42 -0300

    Deletando e recomeçando

--- cursosalura/Java e Orientação a Objetos/Atividades/src/IsNow.java
+++
@@ -1,8 +0,0 @@
-
-public class IsNow {
-	
-	public static void main (String[] args) {
-		
-		System.out.println("Olá caraio");
-	}
-}
--- cursosalura/Java e Orientação a Objetos/Atividades/src/NumeroDecrescente2.java
+++
@@ -1,7 +0,0 @@
-
-public class NumeroDecrescente2 {
-	public static void main(String[] args) {
-		
-		//(int  = ;  <= 10; multiplicador++) 
-	}
-}
