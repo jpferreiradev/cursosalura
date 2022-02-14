3ac7067bb8e58eb46f9ce1c8bbee85fb4206734d
JP Ferreira <jotepauder01@gmail.com>
2022-02-14 16:04:07 -0300

    Update curso de Java

---
+++ cursosalura/Java e Orientação a objetos/sintaxe-variaveis-e-fluxo/src/TestaEscopoVariavel.java
@@ -0,0 +1,34 @@
+
+public class TestaEscopoVariavel {
+
+	public static void main(String args[]) {
+
+		System.out.println("Testando condicional");
+		
+		int idade = 20;
+		int quantidadePessoas = 3;
+		
+		//boolean acompanhado = quantidadePessoas >= 2;
+		
+		boolean acompanhado; 
+		
+		
+		if(quantidadePessoas  >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
+		
+ 		System.out.println("Valor de acompanhado = " + acompanhado);
+		
+		if (idade >= 18 && acompanhado) {
+			System.out.println("Seja bem-vindo");
+		} else {
+			System.out.println("Infelizmente você não pode entrar");
+		}
+	
+	
+	
+	
+	}
+}
