b4ef24ad2ae21e0a918c9e8bc218bed152854a1c
JP Ferreira <jotepauder01@gmail.com>
2022-03-13 12:56:28 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/Atividades/src/exercicioNove.java
+++ cursosalura/Java e Orientação a objetos/Atividades/src/exercicioNove.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
 
 public class exercicioNove {
-		
+
 	public static void main(String args[]) {
-		
-		int numeroInteiro = 5;
-		int numeroAntecessor, numeroSucessor;
-		
-		
-		System.out.println(numeroInteiro);
+
+		int numeroAntecessor = -1;
+		int numeroSucessor = +1;
+
+		Scanner numero = new Scanner(System.in);
+
+		int numeroInteiro;
+		System.out.println("Digite um número inteiro: ");
+		numeroInteiro = numero.nextInt();
+		System.out.println("O número digitado foi: " + numeroInteiro);
+
+		System.out.println("Seu número antecessor é: " + (numeroInteiro + numeroAntecessor));
+		System.out.println("Seu número sucessor é: " + (numeroInteiro + numeroSucessor));
+
 	}
 }
