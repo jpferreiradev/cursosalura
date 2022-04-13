19faf376741e4117477581b97479a30457cd46bf
JP Ferreira <jotepauder01@gmail.com>
2022-04-13 10:23:00 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/exercicios/src/exercicios/AtividadeTres.java
+++ cursosalura/Java e Orientação a objetos/exercicios/src/exercicios/AtividadeTres.java
@@ -1,21 +1,31 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class AtividadeTres {
-	public static void main(String args[] ) {
-		
-		double notaUm = 6;
-		double notaDois = 7;
+	public static void main(String args[]) {
+
+		Scanner numero = new Scanner(System.in);
+		double notaUm;
+		System.out.println("Digite a primeira nota: ");
+		notaUm = numero.nextDouble();
+		System.out.println("Sua primeira nota foi: " + notaUm);
+
+		Scanner numero2 = new Scanner(System.in);
+		double notaDois;
+		System.out.println("Digite a segunda nota: ");
+		notaDois = numero2.nextDouble();
+		System.out.println("A sua segunda nota foi: " + notaDois);
+
 		double calculaMedia = (notaUm + notaDois) / 2;
-		
-		System.out.println(calculaMedia);
-		
-		
-		if(calculaMedia >= 6) {
-			System.out.println("Parabéns você passou");
+
+		if (calculaMedia >= 6) {
+			System.out.println("Você precisa ter uma nota maior ou igual a 6, parabéns você passou!");
 		} else {
-			System.out.println("Você não conseguiu passar");
+			System.out.println("Você não conseguiu passar.");
 		}
-		
-		//System.out.println("A nota da primeira unidade é: " + notaUm);	
-	}	
+
+		System.out.println(calculaMedia);
+
+	}
 }
