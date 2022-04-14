75a69ea89161a3c405c35bf8a2763549deb2ca61
JP Ferreira <jotepauder01@gmail.com>
2022-04-14 18:08:29 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/exercicios/src/exercicios/AtividadeCinco.java
+++ cursosalura/Java e Orientação a objetos/exercicios/src/exercicios/AtividadeCinco.java
@@ -1,12 +1,38 @@
 package exercicios;
 
+import java.util.Scanner;
+import java.lang.Math;
+
+
 public class AtividadeCinco {
 	public static void main(String args[]) {
+
 		
-		int ladoA;
-		int ladoB;
-		int calculoHipotenusa;
+		Scanner entrada = new Scanner(System.in);
+		
+		double valorCatetoA = 6;
+		double valorCatetoB = 8;
+		
+		
+		double calculoRaizQuadradaCatetoA = Math.sqrt(valorCatetoA);
+		double calculoRaizQuadradaCatetoB = Math.sqrt(valorCatetoB);
+		System.out.println(calculoRaizQuadradaCatetoA);
+		System.out.println(calculoRaizQuadradaCatetoB);
+		
+		double calculoHipotenusa = (calculoRaizQuadradaCatetoA + calculoRaizQuadradaCatetoB); 
+		System.out.println(calculoHipotenusa);
+		
 		
 		
 	}
 }
+
+
+/*
+int x = 2;
+Math.sqrt(x);
+System.out.println(x);
+
+
+
+*/
