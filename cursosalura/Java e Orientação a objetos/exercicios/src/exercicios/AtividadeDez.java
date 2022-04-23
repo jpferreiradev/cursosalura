7b840f814bf0b6a27b794f48003f08e27351fe4b
JP Ferreira <jotepauder01@gmail.com>
2022-04-23 10:52:21 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a objetos/exercicios/src/exercicios/AtividadeDez.java
@@ -0,0 +1,10 @@
+package exercicios;
+
+public class AtividadeDez {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ cursosalura/Java e Orientação a objetos/exercicios/src/exercicios/AtividadeNove.java
@@ -0,0 +1,35 @@
+package exercicios;
+
+public class AtividadeNove {
+	
+	public static void main(String args[]) {
+		
+		double numeroUm = 6;
+		double numeroDois = 7; 
+		double numeroTres = 9;
+		double mediaDosDoisMaiores;
+		
+		
+		
+		
+		//System.out.println(mediaDosDoisMaiores);
+		
+		
+		
+		
+	}
+}
+
+
+/*
+
+media aritmetrica => n1+n2+n3 / 3 = media
+
+Faça um programa em Java, para ler 3 números quaisquer e calcular ( imprimir com duas casas decimais)
+a média aritmética dois maiores
+
+
+
+
+
+*/
