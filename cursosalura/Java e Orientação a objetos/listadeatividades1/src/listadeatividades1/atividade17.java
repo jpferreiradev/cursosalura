32beb36c79764cec83d1968f28ccb507b5c84d68
JP Ferreira <jotepauder01@gmail.com>
2022-05-28 14:14:48 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade17.java
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade17.java
@@ -1,8 +1,34 @@
 package listadeatividades1;
 
+import java.util.Scanner;
+
 public class atividade17 {
-		
+
 	public static void main(String args[]) {
-			
+
+		int velocidadeDocarro;
+		double valorMultaPorVelocidade = 5;
+		int valorMaximoDeVelocidade = 80;
+		
+		// double valorMultaVelocidade = velocidadeDocarro * valorMultaPorVelocidade;
+
+		Scanner velocidade = new Scanner(System.in);
+
+		System.out.println("Informe a velocidade que do carro: ");
+		velocidadeDocarro = velocidade.nextInt();
+		System.out.println("A velocidade do carro foi de " + velocidadeDocarro + "km/h");
+
+		double valorDoKmUltrapassado = velocidadeDocarro - valorMaximoDeVelocidade;
+		double valorMultaVelocidadee =  valorDoKmUltrapassado * valorMultaPorVelocidade; 
+	
+		
+		if (velocidadeDocarro > 80) {
+			System.out.println("Você foi multado por ultrapassar 80km/ por hora");
+			System.out.println("O valor da multa por excesso de velocidade foi de R$ " + valorMultaVelocidadee + " reais" );
+		} else {
+			System.out.println("Você não tomou multa por excesso de velocidade.");
+		}
+
+		
 	}
 }
