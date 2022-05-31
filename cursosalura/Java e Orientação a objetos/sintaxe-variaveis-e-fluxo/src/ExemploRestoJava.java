db60ea59caa755954f56da82704d702ca44b52a0
JP Ferreira <jotepauder01@gmail.com>
2022-05-31 17:50:36 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a objetos/sintaxe-variaveis-e-fluxo/src/ExemploRestoJava.java
@@ -0,0 +1,13 @@
+
+public class ExemploRestoJava {
+	public static void main(String args[]) {
+
+		int i = 0;
+		i = (4 % 2); // resto 0
+		System.out.println("Resto da vivisão:" + i);
+		i = (5 % 2); // resto 1
+		System.out.println("Resto da vivisão:" + i);
+		i = (8 % 3); // resto 2
+		System.out.println("Resto da vivisão:" + i);
+	}
+}
