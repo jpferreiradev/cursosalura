e519c7407e60cd2967164cbcdd3f65ea8075deb4
JP Ferreira <jotepauder01@gmail.com>
2022-06-02 15:20:02 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade22.java
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade22.java
@@ -1,7 +1,40 @@
 package listadeatividades1;
 
+import java.util.Scanner;
+
 public class atividade22 {
 	public static void main(String args[]) {
-		
+
+		Scanner idade = new Scanner(System.in);
+
+		System.out.println("Digite o ano atual:");
+		int anoAtual;
+		anoAtual = idade.nextInt();
+
+		int anoDeNascimento;
+		System.out.println("Digite o ano que você nasceu:");
+		anoDeNascimento = idade.nextInt();
+		// System.out.println(anoDeNascimento);
+
+		int valorDaIdade = anoAtual - anoDeNascimento;
+
+		int idadeDoAlistamento = 18;
+		int anosQueFaltamAlistamento = valorDaIdade - idadeDoAlistamento;
+		int anosQuePassaramAlistamento = valorDaIdade - idadeDoAlistamento;
+
+		System.out.println("Você tem " + valorDaIdade + " anos");
+
+		if (valorDaIdade <= idadeDoAlistamento) {
+			System.out.println("Faltam " + anosQueFaltamAlistamento + " anos para você se alistar.");
+		} if (valorDaIdade == idadeDoAlistamento) {
+			System.out.println("Você tem já pode se alistar.");
+		} else {
+			System.out.println("Já se passaram " + anosQuePassaramAlistamento + " anos para você se alistar");
+		}
+
+		// System.out.println("Você tem " + valorDaIdade + " anos");
 	}
 }
+
+//System.out.println(anosQueFaltamAlistamento);
+//System.out.println("Anos que passaram do alistamento " + anosQuePassaramAlistamento);
