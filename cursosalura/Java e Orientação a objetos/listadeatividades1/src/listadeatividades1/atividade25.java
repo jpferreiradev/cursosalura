1f03a7eb000cd67e74ee8a9d6924595e565963bd
JP Ferreira <jotepauder01@gmail.com>
2022-06-08 19:03:40 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade25.java
@@ -0,0 +1,41 @@
+package listadeatividades1;
+
+import java.util.Scanner;
+
+public class atividade25 {
+	public static void main(String args[]) {
+
+		Scanner medida = new Scanner(System.in);
+
+		int ladoA, ladoB, ladoC;
+
+		System.out.println("Digite o tamanho do primeiro lado:");
+		ladoA = medida.nextInt();
+		// System.out.println(ladoA);
+
+		System.out.println("Digite o tamanho do segundo lado:");
+		ladoB = medida.nextInt();
+		// System.out.println(ladoB);
+
+		System.out.println("Digite o tamanho do terceiro lado:");
+		ladoC = medida.nextInt();
+		// System.out.println(ladoC);
+
+		if (ladoA == ladoB && ladoB == ladoC) {
+			System.out.println("Esse triângulo é equilatero");
+		}
+		if (ladoA == ladoB && ladoA != ladoC) {
+			System.out.println("Esse triângulo é isosceles ");
+		} else if (ladoA != ladoB && ladoA != ladoC) {
+			System.out.println("Esse triângulo é escaleno");
+		} else {
+			// System.out.println("Não existe triângulo");
+		}
+
+		 
+
+	}
+}
+
+//System.out.println("O tamanho dos lados são " + ladoA + ladoB + ladoC);
+// Fazer com alguém, ou pedir ajuda;
