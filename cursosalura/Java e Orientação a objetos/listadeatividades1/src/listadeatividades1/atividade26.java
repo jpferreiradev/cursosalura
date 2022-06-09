11b4b0e8003d45773b8f91e7e4ea112b8ef97dad
JP Ferreira <jotepauder01@gmail.com>
2022-06-09 17:03:05 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade26.java
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade26.java
@@ -1,7 +1,32 @@
 package listadeatividades1;
 
+import java.util.Scanner;
+
 public class atividade26 {
 	public static void main(String args[]) {
-		System.out.println("Teste");
+
+		Scanner numero = new Scanner(System.in);
+
+		int numeroUm;
+		System.out.println("Digite o primeiro número:");
+		numeroUm = numero.nextInt();
+		int numeroDois;
+		System.out.println("Digite o segundo número: ");
+		numeroDois = numero.nextInt();
+
+	
+		if(numeroUm > numeroDois) {
+			System.out.println("O primeiro valor é maior");
+		} if (numeroDois > numeroUm) {
+			System.out.println("O segundo valor é maior");
+		} else {
+			System.out.println("Não existe valor maior, os dois são iguais");
+		}
+	
 	}
+
 }
+
+//O primeiro valor é o maior
+//O segundo valor é o maior
+//Não existe valor maior, os dois são iguais
---
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade27.java
@@ -0,0 +1,7 @@
+package listadeatividades1;
+
+public class atividade27 {
+	public static void main(String args[]) {
+		System.out.println("Teste");
+	}
+}
