57bd491810e309011474f2fd02dbbe30651ce777
JP Ferreira <jotepauder01@gmail.com>
2022-06-15 17:06:39 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividadeExtraAlura.java
@@ -0,0 +1,31 @@
+package listadeatividades1;
+
+import java.util.Scanner;
+
+public class atividadeExtraAlura {
+	public static void main(String args[]) {
+
+		for (int contador = 1; contador < 100; contador++) {
+			if (contador % 3 == 0)
+
+				System.out.println(contador);
+		}
+
+	}
+}
+// Scanner nm = new Scanner(System.in);
+
+/*
+ * int numero; System.out.println("Digite um número:"); numero = nm.nextInt();
+ * System.out.println(numero);
+ * 
+ * 
+ * if(numero % 3 == 0) { for(int contador = 0; contador <= 100; contador++) {
+ * System.out.println(contador); } }
+ */
+
+// int numero;
+
+// int numero = 0;
+
+// System.out.println("Esses números são múltiplos de 3");
---
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividadeExtraAluraDois.java
@@ -0,0 +1,14 @@
+package listadeatividades1;
+
+public class atividadeExtraAluraDois {
+	public static void main(String args[]) {
+
+		int fatorial = 1;
+		for (int i = 1; i <= 10; i++) {
+			fatorial *= i;
+			System.out.println("O fatorial de " + i + " é " + fatorial);
+
+		}
+
+	}
+}
