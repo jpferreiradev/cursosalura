003190be727b8b00373db08532438a9a8d20e6b8
JP Ferreira <jotepauder01@gmail.com>
2022-06-20 17:44:09 -0300

    Updat curso Java

--- cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade20.java
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade20.java
@@ -4,31 +4,27 @@ import java.util.Scanner;
 
 public class atividade20 {
 	public static void main(String args[]) {
-			
-	
-		
+
 		Scanner numero = new Scanner(System.in);
-		
+
 		System.out.println("Digite um número: ");
 		int numeroDigitado;
 		numeroDigitado = numero.nextInt();
 		System.out.println("Número digitado foi: " + numeroDigitado);
-		
+
 		// resto 0
 		int numeroPar = numeroDigitado % 2;
-		
-		// resto 1		
+
+		// resto 1
 		int numeroImpar = numeroDigitado % 2;
-		
-		
-		if(numeroPar <= 0) {
+
+		if (numeroPar <= 0) {
 			System.out.println("Esse número é par.");
 		} else {
-			
+
 			System.out.println("Esse número é impar.");
 		}
-		
-		
+
 	}
 }
 
---
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade33.java
@@ -0,0 +1,44 @@
+package listadeatividades1;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class atividade33 {
+	public static void main(String args[]) {
+
+		Scanner csa = new Scanner(System.in);
+
+		DecimalFormat df = new DecimalFormat();
+		//df.applyPattern("R$ #,##0.00");
+		df.applyPattern("#,##0.00");
+		
+		System.out.println("Digite o valor da casa: ");
+		int valorCasa = csa.nextInt();
+		System.out.println("Informe a taxa de juros:");
+		double valorDosJuros = csa.nextDouble();
+		System.out.println("Digite o valor do seu salário:");
+		double valorSalario = csa.nextDouble();
+		System.out.println("Digite em quantos anos você quer pagar a casa:");
+		int valorAnosPagosCasa = csa.nextInt();
+
+		double calculoPorcetagemSalario = (30 * valorSalario) / 100;
+		System.out.println(calculoPorcetagemSalario);
+		System.out.println("O valor da porcetagem do seu salário é de " + df.format(calculoPorcetagemSalario));
+
+		double conversaoJuros = valorDosJuros / 100;
+		double calculoJurosMensal = conversaoJuros / 12;
+		double calculoPrestacaoMensal = calculoJurosMensal * valorCasa;
+		System.out.println(df.format(calculoPrestacaoMensal));
+		
+		if(calculoPorcetagemSalario >= calculoPrestacaoMensal) {
+			System.out.println("Emprestimo permitiado, o seu salário não excedeu os 30%");
+		} else {
+			System.out.println("Infelizmente seu empréstimo será negado");
+		}
+		
+
+		// System.out.println("O valor da casa é R$ " + valorCasa + "reais" + ", o valor
+		// do seu salário é de R$ " + valorSalario + " reais" );
+
+	}
+}
