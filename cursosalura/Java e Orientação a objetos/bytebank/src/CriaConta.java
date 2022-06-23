f20982db6abb27320aef81242646e693bd3184d2
JP Ferreira <jotepauder01@gmail.com>
2022-06-23 09:44:14 -0300

    Update curso Java

---
+++ cursosalura/Java e Orientação a objetos/bytebank/src/CriaConta.java
@@ -0,0 +1,18 @@
+
+public class CriaConta {
+
+	public static void main(String args[]) {
+
+		// criar um objeto tipo conta
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 400;
+		System.out.println(segundaConta.saldo);
+		
+		
+		
+	}
+}
