bad1f2579d3aef8f19d4d997d38536f4d1c99cf9
JP Ferreira <jotepauder01@gmail.com>
2022-08-10 11:58:00 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/sinxtaxe-basica/src/Programa.java
+++ cursosalura/Java e Orientação a objetos/sinxtaxe-basica/src/Programa.java
@@ -1,8 +1,15 @@
-package com.example.hellowolrd
+
 public class Programa {
 	public static void main(String args[]) {
 		System.out.println("Hello, World");
+		System.out.println("Bora volta caraio");
+		System.out.println("Huuhuhu,hahaah!");
 
 
+
+
+
+
+		}
 	}
-}
+
