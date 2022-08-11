9ea675607c4fdba55c576fcafba241b5150f5744
JP Ferreira <jotepauder01@gmail.com>
2022-08-11 18:40:19 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade36.java
+++ cursosalura/Java e Orientação a objetos/listadeatividades1/src/listadeatividades1/atividade36.java
@@ -16,13 +16,35 @@ public class atividade36 {
 		System.out.println("Informe quantas horas foram feitas de atividade:");
 		int quantidadeDeHoras = vsdp.nextInt();
 		System.out.println("A quantidade de horas foi de " + quantidadeDeHoras + " horas");
-		double valorHoraPontoEmReais = quantidadeDeHoras  * valorDoPontoEmReais;
-		System.out.println("A quantidade de pontos ganhos foi de " + quantidadeDeHoras + "e a quantidade de dinheiro foi de " + df.format(valorHoraPontoEmReais));
+		double valorHoraPontoEmReais = quantidadeDeHoras * valorDoPontoEmReais;
+		System.out.println("A quantidade de pontos ganhos foi de " + quantidadeDeHoras + " e a quantidade de dinheiro foi de " + df.format(valorHoraPontoEmReais));
+
+
+		if (quantidadeDeHoras < 10) {
+			valorDoPontoEmReais = 0.10;
+			valorHoraPontoEmReais = quantidadeDeHoras * valorDoPontoEmReais;
+			System.out.println("Como você fez até 10h de atividade você ganhou " + df.format(valorHoraPontoEmReais) + " reais");
+		}
+		if(quantidadeDeHoras > 10 && quantidadeDeHoras < 20) {
+			valorDoPontoEmReais = 0.25;
+			valorHoraPontoEmReais = quantidadeDeHoras * valorDoPontoEmReais;
+			System.out.println("Como você fez mais de 10 até 20h de atividade você ganhou" + df.format(valorHoraPontoEmReais) + " reais");
+
+		} else if (quantidadeDeHoras > 20){
+				valorDoPontoEmReais = 0.50;
+				valorHoraPontoEmReais = quantidadeDeHoras * valorDoPontoEmReais;
+				System.out.println("Como você fez mais de 20h de atividade você ganhou " + df.format(valorHoraPontoEmReais) + " reais");
+			}
+
+			}
+
+		}
 
 
 
 
-	}
-}
+
+
+
 
 
