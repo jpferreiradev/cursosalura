8b9cd4de34b92532d5b6e3783f082257a2289b97
JP Ferreira <jotepauder01@gmail.com>
2022-08-19 14:52:57 -0300

    Update curso Java

--- cursosalura/Java e Orientação a objetos/listadeatividadesOO/src/Pessoa.java
+++ cursosalura/Java e Orientação a objetos/listadeatividadesOO/src/Pessoa.java
@@ -3,6 +3,6 @@ public class Pessoa {
         String nome;
         String cpf;
         int idade;
-        Endereco endereco;
+        Endereco endereco = new Endereco();
 
 }
--- cursosalura/Java e Orientação a objetos/listadeatividadesOO/src/Programa.java
+++ cursosalura/Java e Orientação a objetos/listadeatividadesOO/src/Programa.java
@@ -2,8 +2,10 @@ public class Programa {
     public static void main(String args[]){
 
         Pessoa p = new Pessoa();
+
         p.nome = "Paulo";
         p.endereco.logadouro = "Avenida XYZ";
+        System.out.println(p.endereco.logadouro);
 
     }
 }
