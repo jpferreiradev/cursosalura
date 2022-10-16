e06836570aa50a91e71f4bd9e7ec727e6e806628
JP Ferreira <jotepauder01@gmail.com>
2022-10-15 23:15:24 -0300

    Update curso Java da Alura

---
+++ cursosalura/Java e Orientação a objetos/bytebanck-encapsulado/src/TestaContas.java
@@ -0,0 +1,15 @@
+public class TestaContas {
+    public static void main(String args[]){
+
+        Conta conta = new Conta(1337,2334);
+
+        conta.deposita(200.0);
+
+        System.out.println(conta.getSaldo());
+        conta.setAgencia(570);
+
+        System.out.println(conta.getAgencia());
+
+        System.out.println("O total de contas é : " + Conta.getTotal());
+    }
+}
