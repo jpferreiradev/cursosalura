bc4201ea3e33a79f2a7d0c92118f17af6d1a1a3a
JP Ferreira <jotepauder01@gmail.com>
2022-10-18 15:05:37 -0300

    Update curso Java POO

---
+++ cursosalura/Java e Orientação a objetos/bytebank-herdado/src/Designer.java
@@ -0,0 +1,7 @@
+public class Designer extends Funcionario {
+
+    public double getBonicacao() {
+        System.out.println("Chamando o método de bonificacao do Designer");
+        return 200;
+    }
+}
--- cursosalura/Java e Orientação a objetos/bytebank-herdado/src/TesteReferencias.java
+++ cursosalura/Java e Orientação a objetos/bytebank-herdado/src/TesteReferencias.java
@@ -14,11 +14,14 @@ public class TesteReferencias {
         EditorVideo ev = new EditorVideo();
         ev.setSalario(2500.0);
 
+        Designer ds = new Designer();
+        ds.setSalario(2000.0);
 
         ControleBonificacao controle = new ControleBonificacao();
         controle.registra(g1);
         controle.registra(f);
         controle.registra(ev);
+        controle.registra(ds);
 
         System.out.println(controle.getSoma());
 
