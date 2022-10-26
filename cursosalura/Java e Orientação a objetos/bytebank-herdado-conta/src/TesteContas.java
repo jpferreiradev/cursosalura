53e04b318088f9b73fd969f369b53d821cf48833
JP Ferreira <jotepauder01@gmail.com>
2022-10-26 18:20:31 -0300

    Update curso Java POO

--- cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/TesteContas.java
+++ cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/TesteContas.java
@@ -1,8 +1,16 @@
 public class TesteContas {
     public static void main(String[] args) {
 
+        //int a = 3;
+        //int b = a / 0;
+        //System.out.println(b);
+
         ContaCorrente cc = new ContaCorrente(111,111);
         ContaCorrente ff = new ContaCorrente(777,4567);
+        //ContaCorrente outra = null;
+
+        //outra.deposita(200.0);
+
         cc.deposita(100.0);
         ff.deposita(400.0);
 
--- cursosalura/Java e Orientação a objetos/java-pilha/src/Fluxo.java
+++ cursosalura/Java e Orientação a objetos/java-pilha/src/Fluxo.java
@@ -2,19 +2,44 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Inicialização do main"); // Passo 1
-        metodo1(); // Passo 2
+        try {
+            metodo1(); // Passo 2
+        } catch (ArithmeticException | NullPointerException ex){
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
+        }
         System.out.println("Fim do main"); // Passo 3
     }
-    private static void metodo1(){
+
+    private static void metodo1() {
         System.out.println("Inicialização do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
-    private static void metodo2(){
-        System.out.println("Inicialização d o metodo2");
-        for(int i = 1; i <= 5;i++){
+
+    private static void metodo2() {
+        System.out.println("Inicialização do metodo2");
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
+            int a = i / 0;
+            //Conta c = null;
+            //c.deposita();
         }
-        System.out.println("Fim do metodo2");
+            System.out.println("Fim do metodo2");
+        }
     }
-}
+
+/*
+
+} catch (ArithmeticException | NullPointerException ex){ // Nova forma para ter varios catchs
+            //String msg = ex.getMessage();
+            //System.out.println("ArithmeticException" + msg);
+            ex.printStackTrace();
+        } catch (NullPointerException ex){
+            String msg = ex.getMessage();
+            System.out.println("NullPointerException - Isso aqui é uma exceção" + msg);
+            //ex.printStackTrace();
+
+ */
+
