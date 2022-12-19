073fcdd46449c96c5c30cc10818cb312b860b249
JP Ferreira <jotepauder01@gmail.com>
2022-12-19 16:31:51 -0300

    Update curso Java e suas bibliotecatas

--- cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
+++ cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -1,21 +1,59 @@
 package br.com.bytebank.banco.test;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class TesteString {
 	
 	public static void main(String args[]) {
 					
 		
 		
+		String teste = "Bora";
+		//CharSequence seq = "é uma sequencia de caractgeres";
+		
+		
+		String vazioDois = "Alura";
+		System.out.println(vazioDois.contains("Alu"));
+		
+		
+		String vazio = " Alura ";
+		String outroVazio = vazio.trim();
+		
+		
+		System.out.println(vazio);
+		System.out.println(outroVazio); // Ele tirou os espaços da String
+		
+		//  Quando algum usuario esqueceu de preencher alguma formulario
+		//System.out.println(outroVazio.isEmpty());
+		
+		
+		
+		
 		String nome = "Alura"; // [object literal] // Nessa situação em letra maiscula é um Classe;
 	
+
+		System.out.println(nome.length());
 		
-		//String outra = nome.replace("A", "a");
+		for(int i = 0; i < nome.length(); i++) {
+			System.out.println(nome.charAt(i));
+		}
 		
 		
-		String outra = nome.toLowerCase();
+//		String sub = nome.substring(1);
+//		System.out.println(sub);
 		
-		System.out.println(nome);
-		System.out.println(outra);
+//		int pos = nome.indexOf("ur");
+//		System.out.println(pos);
+		
+		
+		
+		//		char c = nome.charAt(0);
+//		System.out.println(c);
+		
+		//System.out.println(nome);
+		//System.out.println(outra);
+		
+		
 		
 		
 		
@@ -24,3 +62,13 @@ public class TesteString {
 
 //String outro = new String("Alura"); -> Aqui é considerado má prática.
 //int a = 3; // O int nessa situação é uma palavra chave
+
+
+
+//char c = 'A';
+//char d = 'a';
+//String outra = nome.replace(c,d);
+
+
+//String outra = nome.toLowerCase();
+//String outraDois = nome.toUpperCase();
