03b92f910cad806a43de01d90091232b2e719acd
JP Ferreira <jotepauder01@gmail.com>
2022-12-22 17:15:09 -0300

    Aprendendo mais sobre a classe Objcet com Arrays

---
+++ cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayDePrimitivos.java
@@ -0,0 +1,42 @@
+package br.com.bytebank.banco.test;
+
+public class Teste {
+
+    // Array[]
+    public static void main(String[] args) {
+
+
+        int idades [] = new int[5]; // inicializa o array com os valores padrões
+
+        // Exemplo de arrays com laços
+        for(int i = 0; i < idades.length;i++){
+            idades[i] = i * i;
+
+        }
+
+        for(int i = 0; i < idades.length; i++){
+            System.out.println(idades[i]);
+        }
+
+    }
+}
+
+
+/*
+
+idades[0] = 29;
+        idades[1] = 45;
+        idades[2] = 56;
+        idades[3] = 67;
+        idades[4] = 78;
+
+        System.out.println(idades.length);
+
+        //System.out.println("O tamanho do meu Array é de " + idades.length);
+
+
+
+ */
+
+
+
---
+++ cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -0,0 +1,2 @@
+package br.com.bytebank.banco.test;public class TesteArrayReferencias {
+}
