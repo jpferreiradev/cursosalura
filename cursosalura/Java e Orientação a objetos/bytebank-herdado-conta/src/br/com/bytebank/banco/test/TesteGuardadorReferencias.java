426b8894502150e3f983db15941bfa2214784929
JP Ferreira <jotepauder01@gmail.com>
2022-12-28 17:58:07 -0300

    Aprendendo o sobre a classe java.util.list

--- cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteGuardadorReferencias.java
+++ cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteGuardadorReferencias.java
@@ -2,32 +2,24 @@ package br.com.bytebank.banco.test;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.GuardadorDeContas;
+import br.com.bytebank.banco.modelo.GuardadorDeReferencias;
 
-public class Teste {
-
+public class TesteGuardadorReferencias {
     public static void main(String[] args) {
 
-        GuardadorDeContas guardador = new GuardadorDeContas();
+        GuardadorDeReferencias guardador = new GuardadorDeReferencias();
 
         Conta cc = new ContaCorrente(22,11);
         guardador.adiciona(cc);
 
-        Conta cc2 = new ContaCorrente(22,55);
+        Conta cc2 = new ContaCorrente(22,22);
         guardador.adiciona(cc2);
 
-        Conta cc3 = new ContaCorrente(67,45);
-        guardador.adiciona(cc3);
-
-
-        int tamanho = guardador.getQuantidadeElementos();
+        int tamanho = guardador.getQuantidadeDeElementos();
         System.out.println(tamanho);
 
-        Conta referencia = guardador.getReferencia(2);
+        Conta referencia = (Conta) guardador.getReferencia(1);
         System.out.println(referencia.getNumero());
 
     }
-
-
 }
-
