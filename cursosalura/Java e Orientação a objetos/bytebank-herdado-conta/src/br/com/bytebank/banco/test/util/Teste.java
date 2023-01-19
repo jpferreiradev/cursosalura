1264954981c742da974485466d5a219150a4ee69
JP Ferreira <jotepauder01@gmail.com>
2023-01-19 16:55:20 -0300

    Finalização do curso base HTML

--- cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ cursosalura/Java e Orientação a objetos/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -2,6 +2,7 @@ package br.com.bytebank.banco.test.util;
 
 import java.util.ArrayList;
 
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 
@@ -9,41 +10,43 @@ public class Teste {
 
     public static void main(String[] args) {
 
-        ArrayList lista = new ArrayList();
+       ArrayList lista = new ArrayList();
 
-        Conta cc = new ContaCorrente(22, 11);
-        lista.add(cc);
+        //Conta cc = new ContaCorrente(22,11);
+        Cliente cliente = new Cliente();
+        lista.add(cliente);
 
         Conta cc2 = new ContaCorrente(22,22);
         lista.add(cc2);
 
-        System.out.println("Tamanho: " + lista.size()); // Verificar o tamanho do Array
 
-        Conta ref =(Conta) lista.get(0);
-        System.out.println(ref.getNumero() + " e a agência " + ref.getAgencia());
+        System.out.println("Tamanho: " + lista.size());
+        //Object ref =   lista.get(0); -> Transformando referencia generica e uma referencia especifica
+        Conta ref = (Conta) lista.get(0);
+        System.out.println(ref.getNumero());
 
         lista.remove(0);
-        System.out.println("Tamanho: " + lista.size());
+        System.out.println("Tamanho:" + lista.size());
 
-        Conta cc3 = new ContaCorrente(44,66);
+        Conta cc3 = new ContaCorrente(33,311);
         lista.add(cc3);
-        Conta cc4 = new ContaCorrente(77,99);
+
+        Conta cc4 = new ContaCorrente(33, 332);
         lista.add(cc4);
 
 
-        // Forma legada
-        for(int i = 0; i<lista.size();i++){
-            Object oRef = lista.get(i);
+        //  for, código legado
+        for(int i = 0;i < lista.size(); i++){
+           Object oRef = lista.get(i);
             System.out.println(oRef);
         }
 
-        System.out.println(" ----------");
+        System.out.println("-------------");
 
-        // Forma nova
+        // Forma nova de usar o for hoje me dia
         for(Object oRef : lista){
             System.out.println(oRef);
         }
-
 
 
     }
